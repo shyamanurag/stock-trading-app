4a817790980b607e0a98108a2428c75b024126f0
shyamanurag <ranchissi@gmail.com>
2025-05-13 14:39:35 +0530

    Create AdminWalletPage.jsx

---
+++ src/pages/admin/AdminWalletPage.jsx
@@ -0,0 +1,10 @@
+// src/pages/admin/AdminWalletPage.jsx
+
+import React from 'react';
+import AdminRechargeInterface from '../../components/admin/AdminRechargeInterface';
+
+const AdminWalletPage = () => {
+  return <AdminRechargeInterface />;
+};
+
+export default AdminWalletPage;
