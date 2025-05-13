37b4d34f8676afa1e954dc3a078de8cd058bd1a9
shyamanurag <ranchissi@gmail.com>
2025-05-13 18:31:25 +0530

    Create AnalyticsDashboardPage.jsx

---
+++ src/components/analytics/AnalyticsDashboardPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AdvancedAnalyticsDashboard from '../../components/analytics/AdvancedAnalyticsDashboard';
+
+const AnalyticsDashboardPage = () => {
+  return (
+    <div className="container mx-auto py-6">
+      <AdvancedAnalyticsDashboard />
+    </div>
+  );
+};
+
+export default AnalyticsDashboardPage;
